bf4bdb4de05f1f2fc6ebdd4adf98d477b8d094cc
FernandoRibeiro96 <lluppribeiro1996@gmail.com>
2022-09-15 11:49:50 -0300

    App com Retrofit e arquitetura mvvm.

---
+++ app/src/main/java/com/fernandoribeiro/mvvm/views/MainActivity.java
@@ -0,0 +1,16 @@
+package com.fernandoribeiro.mvvm.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fernandoribeiro.mvvm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
